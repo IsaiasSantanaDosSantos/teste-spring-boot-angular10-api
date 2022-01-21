c9c7343b90a8f5049c1618ac12fbd61c29ab9abe
Isaias Santana dos Santos <isaiassantanadossantos@hotmail.com>
2022-01-21 14:09:24 -0300

    Commit inicial

---
+++ src/test/java/com/isaias/teste/TesteSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isaias.teste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
